8296e971c23f0c6ff357ff6031be7deea87a9b84
Archie Healy <archiethehead@gmail.com>
2026-09-21 21:12:12 +0100

    wcl & include
    Chore: add an include path to the compile command, as well as switching
    it out for it's recursive alternative to compile multiple files.
    
    Chore: Begin the KDOS implementation of conio.h

--- KDOS/kernel/kernel.c
+++ KDOS/kernel/kernel.c
@@ -1,4 +1,8 @@
-void print_char(char c) {
+#include "conio.h"
+
+char* CRLF = "\n\r";
+
+void printChar(char c) {
 
     __asm {
 
@@ -7,41 +11,60 @@ void print_char(char c) {
         int 0x10
 
     }
-    
+
 }
 
-char blocking_input() {
+char blockingInput() {
 
     char input = 0;
 
     __asm {
+
         mov ah, 0x00
         int 0x16
         mov input, al
+        
     }
 
     return input;
 
 }
 
-void print_string(char* string) {
+void printString(char* string) {
+
+    register unsigned short incrementCount = 0;
 
     while (*string != '\0') {
 
-        print_char(*string);
+        printChar(*string);
         string++;
+        incrementCount++;
 
     }
+
+    string -= incrementCount;
 
 }
 
 // This function will sit at the absolute top of the binary
 void kernelMain(void) {
-    
+
+    clrscr();
+
     while (1) {
 
-        char userInput = blocking_input();
-        print_char(userInput);
+        char userInput = blockingInput();
+
+        switch (userInput) {
+
+        case (0x0D):
+            printString(CRLF);
+            break;
+
+        default:
+            printChar(userInput);
+
+        }
 
     }
 
--- run.bat
+++ run.bat
@@ -6,8 +6,9 @@ if not exist build\ (
 
 nasm -f bin ./KDOS/boot.asm -o ./build/boot.bin || goto :exit
 nasm -f obj ./KDOS/kernel/kernel-entry.asm -o ./build/kernel-entry.obj || goto :exit
-wcc -0 -d0 -ms -s -wx -zl  ./KDOS/kernel/kernel.c -fo=./build/kernel.obj || goto :exit
-wlink FILE ./build/kernel-entry.obj,./build/kernel.obj FORMAT RAW BIN NAME ./build/kernel.bin OPTION NODEFAULTLIBS, START=kernelMain_ || goto :exit
+set INCLUDE=./KDOS/kernel/include
+wcl -c -0 -d0 -ms -s -wx -zl ./KDOS/kernel/utils/conio.c ./KDOS/kernel/kernel.c -fo=./build/|| goto :exit
+wlink FILE ./build/kernel-entry.obj,./build/conio.obj,./build/kernel.obj FORMAT RAW BIN NAME ./build/kernel.bin OPTION NODEFAULTLIBS, START=_cstart_ || goto :exit
 "Build Tool" || goto :exit
 
 qemu-system-x86_64 -boot c -drive file=build/KDOS.img,format=raw
